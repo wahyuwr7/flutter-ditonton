f7f86fb9204806527e1c7002b08ee4dc1392d8c2
wahyuwr7 <wahyu.wrseven@gmail.com>
2023-01-21 09:53:15 +0800

    migrate state in popular page to bloc

--- feature_movie/lib/presentation/pages/popular_movies_page.dart
+++ feature_movie/lib/presentation/pages/popular_movies_page.dart
@@ -1,8 +1,12 @@
 import 'package:core/utils/state_enum.dart';
+import 'package:movie/presentation/bloc/movie_event.dart';
+import 'package:movie/presentation/bloc/movie_popular_bloc/movie_popular_bloc.dart';
+import 'package:movie/presentation/bloc/movie_state.dart';
 import 'package:movie/presentation/provider/popular_movies_notifier.dart';
 import 'package:movie/presentation/widgets/movie_card_list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PopularMoviesPage extends StatefulWidget {
   static const ROUTE_NAME = '/movie/popular';
@@ -15,9 +19,7 @@ class _PopularMoviesPageState extends State<PopularMoviesPage> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(() =>
-        Provider.of<PopularMoviesNotifier>(context, listen: false)
-            .fetchPopularMovies());
+    context.read<MoviePopularBloc>().add(FetchingPopularMovies());
   }
 
   @override
@@ -28,24 +30,29 @@ class _PopularMoviesPageState extends State<PopularMoviesPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Consumer<PopularMoviesNotifier>(
-          builder: (context, data, child) {
-            if (data.state == RequestState.Loading) {
-              return Center(
+        child: BlocBuilder<MoviePopularBloc, MovieState>(
+          builder: (context, state) {
+            if (state is MovieStateLoading) {
+              return const Center(
                 child: CircularProgressIndicator(),
               );
-            } else if (data.state == RequestState.Loaded) {
+            } else if (state is MovieStateHasData) {
+              final data = state.result;
               return ListView.builder(
                 itemBuilder: (context, index) {
-                  final movie = data.movies[index];
+                  final movie = data[index];
                   return MovieCard(movie);
                 },
-                itemCount: data.movies.length,
+                itemCount: data.length,
+              );
+            } else if(state is MovieStateError){
+              return Center(
+                key: const Key('error_message'),
+                child: Text(state.message),
               );
             } else {
-              return Center(
-                key: Key('error_message'),
-                child: Text(data.message),
+              return const Center(
+                child: Text("Unknown Error"),
               );
             }
           },
